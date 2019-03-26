9a99bafae2f6ae71c43d5fdb270e6874e02356f0
WajdMariam99 <47641469+WajdMariam99@users.noreply.github.com>
2019-03-26 13:27:17 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,7 +18,7 @@ LargePizza.y = 1300
 LargePizza.id = "Large Pizza"
 
 local NumOfToppingsTextfield = native.newTextField (display.contentCenterX, display.contentCenterY + 100, native.systemFont, 90 )
-NumOfToppingsTextfield.id = "Number of Topppings"
+NumOfToppingsTextfield.id = "Number of Toppings"
 
 local ToppingTextfield = display.newText("Enter Number of Toppings", display.contentCenterX,display.contentCenterY -90, native.systemFont, 90)
 ToppingTextfield.id = "ToppingTextfield"
@@ -31,14 +31,70 @@ end
 
 local function calculatePrice (event)
 	local tax = subtotal * 1.13
+end
 
 local function onLargetouch( event )
 	
 	Toppings = tonumber (NumOfToppingsTextfield)
 
-	if toppings == 1
+	if toppings == 1 then
+		subtotal = 6 + 1
+		calculatePrice ()
+
+	elseif toppings == 0 then
+		subtotal = 6 + 0
+		calculatePrice ()
+
+	elseif toppings == 2 then
+		subtotal = 6 + 1.75
+		calculatePrice ()
+
+	elseif toppings == 3 then
+		subtotal = 6 + 2.5
+		calculatePrice ()
+	
+	elseif toppings == 4 then
+		subtotal = 6 + 3.35
+		calculatePrice ()
+
+    local errorText = display.newText('Too many Toppings', display.contentCenterX + 20, display.contentCenterY - 600, native.systemFont, 70)
+errorText:setTextColor ( 0, 0, 255)
+    end 
+
 end
 
+local function onExtraLargeTouch (event)
+	
+	Toppings = tonumber (NumOfToppingsTextfield)
+
+	if toppings == 1 then 
+		subtotal = 10 + 1
+		calculatePrice ()
+
+    elseif toppings == 0 then
+    	subtotal = 10 + 0
+    	calculatePrice ()
+
+    elseif toppings == 2 then
+    	subtotal = 10 + 1.75
+    	calculatePrice ()
+
+    elseif toppings == 3 then
+    	subtotal = 10 + 2.5
+    	calculatePrice ()
+
+    elseif toppings == 4 then
+    	subtotal = 10 + 3.35
+    	calculatePrice ()
+
+    local errorText = display.newText('Too many Toppings', display.contentCenterX + 20, display.contentCenterY - 600, native.systemFont, 70)
+errorText:setTextColor ( 0, 0, 255)
+    end 
+
+end 
+
+LargePizza: addEventListener ('touch', onLargetouch)
+XtraLargePizza: addEventListener ('touch', onExtraLargeTouch)
 
 
 
