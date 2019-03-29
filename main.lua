c6990a1589727f1a81be9c80e9688ced8fcda6a1
WajdMariam99 <47641469+WajdMariam99@users.noreply.github.com>
2019-03-29 13:31:05 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,59 +1,65 @@
------------------------------------------------------------------------------------------
+---------------------------------------------------------
 --
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
-local CostTextfield = display.newText( "Cost of Pizza", display.contentCenterX, display.contentCenterY - 100 , native.systemFont, 30 )
-CostOfPizzaTextField:setFillColor( 87, 61, 178 )
+local Cost = display.newText( "Cost of Pizza", display.contentCenterX, display.contentCenterY - 150 , native.systemFont, 30 )
+Cost.id = "Cost of Pizza"
 
-local XtraLargePizza = display.newImageRect( "./assets,sprites,XtraLargePizza.PNG", 406, 157 )
-XtraLargePizza.x = 250
-XtraLargePizza.y = 1300
-XtraLargePizza.id = "Xtra Large Pizza"
+local XtraLargeButton = display.newImageRect( "./XtraLargeButton.png", 125, 125 )
+XtraLargeButton.x = 250
+XtraLargeButton.y = 400
+XtraLargeButton.id = "Xtra Large Button"
 
 
-local LargePizza = display.newImageRect( "./assets,sprites,LargePizza.PNG", 406, 157 )
-LargePizza.x = 250
-LargePizza.y = 1300
-LargePizza.id = "Large Pizza"
+local LargeButton = display.newImageRect( "./LargeButton.png", 125, 125 )
+LargeButton.x = 75
+LargeButton.y = 400
+LargeButton.id = "Large Button"
 
-local NumOfToppingsTextfield = native.newTextField (display.contentCenterX, display.contentCenterY + 100, native.systemFont, 90 )
+local NumOfToppingsTextfield = native.newTextField(160,250,150,40)
 NumOfToppingsTextfield.id = "Number of Toppings"
 
-local ToppingTextfield = display.newText("Enter Number of Toppings", display.contentCenterX,display.contentCenterY -90, native.systemFont, 90)
-ToppingTextfield.id = "ToppingTextfield"
-ToppingTextfield:setFillColor ( 0, 255, 0 )
+local Topping = display.newText("Enter Number of Toppings", display.contentCenterX,display.contentCenterY -50, native.systemFont, 20)
+Topping.id = "Toppings"
+
 
 function round(num, numDecimalPlaces)
   local mult = 10^(numDecimalPlaces or 2)
   return math.floor(num * mult + 0.5) / mult
 end
 
+local Cost
+local XtraLargeButton
+local LargeButton
+local NumOfToppingsTextfield
+local Topping
+
 local function calculatePrice (event)
 	local tax = subtotal * 1.13
 end
 
-local function onLargetouch( event )
+local function LargeButtonTouch( event ) 
 	
 	Toppings = tonumber (NumOfToppingsTextfield)
 
-	if toppings == 1 then
+	if Toppings == 1 then
 		subtotal = 6 + 1
 		calculatePrice ()
 
-	elseif toppings == 0 then
+	elseif Toppings == 0 then
 		subtotal = 6 + 0
 		calculatePrice ()
 
-	elseif toppings == 2 then
+	elseif Toppings == 2 then
 		subtotal = 6 + 1.75
 		calculatePrice ()
 
-	elseif toppings == 3 then
+	elseif Toppings == 3 then
 		subtotal = 6 + 2.5
 		calculatePrice ()
 	
-	elseif toppings == 4 then
+	elseif Toppings == 4 then
 		subtotal = 6 + 3.35
 		calculatePrice ()
 
@@ -63,27 +69,27 @@ errorText:setTextColor ( 0, 0, 255)
 
 end
 
-local function onExtraLargeTouch (event)
+local function XtraLargeButtonTouch (event)
 	
 	Toppings = tonumber (NumOfToppingsTextfield)
 
-	if toppings == 1 then 
+	if Toppings == 1 then 
 		subtotal = 10 + 1
 		calculatePrice ()
 
-    elseif toppings == 0 then
+    elseif Toppings == 0 then
     	subtotal = 10 + 0
     	calculatePrice ()
 
-    elseif toppings == 2 then
+    elseif Toppings == 2 then
     	subtotal = 10 + 1.75
     	calculatePrice ()
 
-    elseif toppings == 3 then
+    elseif Toppings == 3 then
     	subtotal = 10 + 2.5
     	calculatePrice ()
 
-    elseif toppings == 4 then
+    elseif Toppings == 4 then
     	subtotal = 10 + 3.35
     	calculatePrice ()
 
@@ -93,8 +99,12 @@ errorText:setTextColor ( 0, 0, 255)
 
 end 
 
-LargePizza: addEventListener ('touch', onLargetouch)
-XtraLargePizza: addEventListener ('touch', onExtraLargeTouch)
+
+LargeButton: addEventListener ("touch" , onLargeButtonTouch)
+
+XtraLargeButton: addEventListener ("touch" , onXtraLargeButtonTouch)
+ 
+
 
 
 
